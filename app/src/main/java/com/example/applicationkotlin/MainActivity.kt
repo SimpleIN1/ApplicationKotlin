4996e8a24bc2fbe97eb23c3af80bcafb86ad6ead
grigoriy <zimforshint_akross@mail.ru>
2022-10-11 17:49:32 +0300

    the calculator is ready

--- app/src/main/java/com/example/applicationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/applicationkotlin/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.applicationkotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doBeforeTextChanged
+import androidx.core.widget.doOnTextChanged
 
 
 enum class Signs(val sign:String){
@@ -15,55 +19,93 @@ enum class Signs(val sign:String){
 
 class MainActivity : AppCompatActivity() {
 
-    private fun setText(value: Int, text: TextView){
-        text.text = value.toString();
+
+    private fun get_valid_value (str: String): String {
+        return if ( str == "") "0" else str
     }
 
+    private fun execute_action(spinner: Spinner,first_value: Double, second_value: Double): Double{
+        when(spinner.selectedItem){
+            Signs.PLUS.sign -> {
+                return first_value + second_value
+            }
+            Signs.MINUS.sign -> {
+                return first_value - second_value
+            }
+            Signs.DIVIDE.sign -> {
+                return first_value / second_value
+            }
+            Signs.MULTIPLY.sign -> {
+                return first_value * second_value
+            }
+            else -> {
+                return 0.0
+            }
+        }
+    }
+
+    @SuppressLint("ResourceType", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val spinner = findViewById<Spinner>(R.id.spinner2);
-
-
         val list_potions = ArrayList<String>();
 
         Signs.values().forEach {
             list_potions.add(it.sign)
         }
 
-        val adapter =  ArrayAdapter(this, android.R.layout.simple_spinner_item,list_potions);
+        val adapter =  ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            list_potions
+        );
 
         spinner.adapter = adapter;
 
 
-        val value = findViewById<EditText>(R.id.editTextNumber3);
-        val value1 = findViewById<EditText>(R.id.editTextNumber4);
+        val first_value = findViewById<EditText>(R.id.editTextNumber4);
+        val second_value = findViewById<EditText>(R.id.editTextNumber3);
+        val textView = findViewById<TextView>(R.id.textView);
 
-        var sum = 0;
+
+        first_value.doAfterTextChanged {
+            textView.text = execute_action(
+                spinner,
+                get_valid_value(first_value.text.toString()).toDouble(),
+                get_valid_value(second_value.text.toString()).toDouble()
+            ).toString();
+        }
+
+        second_value.doAfterTextChanged {
+            textView.text = execute_action(
+                spinner,
+                get_valid_value(first_value.text.toString()).toDouble(),
+                get_valid_value(second_value.text.toString()).toDouble()
+            ).toString();
+        }
+
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // ничего не выбрали
-            }
-
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
                 position: Int,
                 id: Long
             ) {
-                println(spinner.selectedItem);
-                when(spinner.selectedItem){
-                    Signs.PLUS.sign -> println(1)
-                    Signs.MINUS.sign -> println(2)
-                    Signs.DIVIDE.sign -> println(3)
-                    Signs.MULTIPLY.sign -> println(4)
-                }
+                textView.text = execute_action(
+                    spinner,
+                    get_valid_value(first_value.text.toString()).toDouble(),
+                    get_valid_value(second_value.text.toString()).toDouble()
+                ).toString();
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
             }
         }
-
-
     }
 }
+
 
